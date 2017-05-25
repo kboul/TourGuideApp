c34f52c3905380f61a1a2c5361b0fa1a88433e6f
kboul <kos.voulgaridis@gmail.com>
2017-05-25 21:13:51 +0300

    Add ViewPager & TabLayout

---
+++ app/src/main/java/com/example/android/tourguideapp/TourGuideFragmentPagerAdapter.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.tourguideapp;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Provides the appropriate {@link Fragment} for a view pager.
+ */
+public class TourGuideFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    private Context context;
+
+    public TourGuideFragmentPagerAdapter(Context context, FragmentManager fm) {
+
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new AttractionsFragment();
+        } else if (position == 1) {
+            return new RestaurantsFragment();
+        } else if (position == 2) {
+            return new BarsFragment();
+        } else {
+            return new HotelsFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return context.getString(R.string.category_attractions);
+        } else if (position == 1) {
+            return context.getString(R.string.category_restaurants);
+        } else if (position == 2) {
+            return context.getString(R.string.category_bars);
+        } else {
+            return context.getString(R.string.category_hotels);
+        }
+    }
+}
